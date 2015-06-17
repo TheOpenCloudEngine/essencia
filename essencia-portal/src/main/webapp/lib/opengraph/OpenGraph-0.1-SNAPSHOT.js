54cc835009a59b5ad649a4beb9e10df02993e5cc
idtndhksha <idtndhksha@uengine.org>
2015-06-17 18:30:17 +0900

    오픈그래프 label에 도형 fillcolor로 background-color 추가

--- essencia-portal/src/main/webapp/lib/opengraph/OpenGraph-0.1-SNAPSHOT.js
+++ essencia-portal/src/main/webapp/lib/opengraph/OpenGraph-0.1-SNAPSHOT.js
@@ -16452,8 +16452,8 @@ OG.renderer.RaphaelRenderer.prototype._drawLabel = function (position, text, siz
 	});
 	
 	$(element.node).css({
-		"text-align" : "center"
-		,"cursor"	 : "move"
+		"cursor"	 : "move",
+        "background-color" : _style["fill"] || me._CONFIG.DEFAULT_STYLE.LABEL["fill"]
 	});
 
 	// angle 적용
@@ -25715,7 +25715,7 @@ OG.graph.Canvas = function (container, containerSize, backgroundColor, backgroun
 			TERMINAL_OVER : { stroke: "#0077FF", "stroke-width": 0, fill: "r(0.5, 0.5)#FFFFFF-#0077FF", "fill-opacity": 1, cursor: "pointer" },
 			TERMINAL_BBOX : { stroke: "none", fill: "white", "fill-opacity": 0 },
 			DROP_OVER_BBOX: { stroke: "#0077FF", fill: "none", opacity: 0.3, "shape-rendering": "crispEdges" },
-			LABEL         : { "font-size": 12, "font-color": "black" },
+			LABEL         : { "font-size": 12, "font-color": "black", "fill": "white" },
 			LABEL_EDITOR  : { position: "absolute", overflow: "visible", resize: "none", "text-align": "center", display: "block", padding: 0 },
 			COLLAPSE      : { stroke: "black", fill: "none", "fill-opacity": 0, cursor: "pointer", "shape-rendering": "crispEdges" },
 			COLLAPSE_BBOX : { stroke: "none", fill: "none", "fill-opacity": 0 },
