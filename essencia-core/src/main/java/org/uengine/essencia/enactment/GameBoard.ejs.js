af9111403a97d7ee1d56f61d0175e8d71c11ecad
SeungpilPark <sppark@uengine.org>
2016-03-14 18:10:59 +0900

    Tooltip class refresh on GameBoard Item click

---
+++ essencia-core/src/main/java/org/uengine/essencia/enactment/GameBoard.ejs.js
@@ -0,0 +1,9 @@
+var org_uengine_essencia_enactment_GameBoard = function(objectId, className){
+
+    $('.tooltip-top').tooltip({
+        placement: 'top',
+        viewport: {selector: 'body', padding: 2}
+    });
+
+};
+
