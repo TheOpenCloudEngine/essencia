76f7f0bcdc958f7d5321fbfaa247b69e81cccf21
jinyoung jang <jyjang@uengine.org>
2016-05-27 10:44:48 +0900

    Changing the Error message when to amend connected alpha kernel.

--- essencia-core/src/main/java/org/uengine/essencia/model/face/list/StateListFace.java
+++ essencia-core/src/main/java/org/uengine/essencia/model/face/list/StateListFace.java
@@ -2,6 +2,7 @@ package org.uengine.essencia.model.face.list;
 
 import org.metaworks.MetaworksContext;
 import org.metaworks.annotation.AutowiredFromClient;
+import org.metaworks.annotation.Order;
 import org.metaworks.annotation.ServiceMethod;
 import org.uengine.essencia.enactment.EssenceProcessDefinition;
 import org.uengine.essencia.model.Alpha;
@@ -17,6 +18,7 @@ public class StateListFace extends ListFace<State> {
 
 
     @ServiceMethod(callByContent = true)
+    @Order(100)
     public void amendFromKernel(@AutowiredFromClient Alpha alpha, @AutowiredFromClient MethodEditor methodEditor){
 
         PracticeDefinition practiceDefinition = methodEditor.createPracticeDefinition();
@@ -26,9 +28,16 @@ public class StateListFace extends ListFace<State> {
         if(alpha==null)
             throw new RuntimeException("Please apply once before amending states from the kernel and try it after opening the properties dialog again.");
 
-        if(alpha.getIncomingRelations()==null || alpha.getIncomingRelations().size() == 0){
+        if(!(alpha instanceof Alpha) || alpha.getIncomingRelations()==null || alpha.getIncomingRelations().size() == 0){
             throw new RuntimeException("There is no parent alpha to amend state.");
         }
+
+
+        if(!(alpha.getIncomingRelations().get(0).getSourceElement() instanceof Alpha)){
+            throw new RuntimeException("There is no parent alpha to amend state.");
+        }
+
+
 
         Alpha kernel = (Alpha) alpha.getIncomingRelations().get(0).getSourceElement();
         if(kernel!=null){
