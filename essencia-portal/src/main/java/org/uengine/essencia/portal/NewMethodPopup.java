569242f7824aa577f117df72c1aea88256eacdd4
jinyoung jang <jyjang@uengine.org>
2016-05-27 10:47:58 +0900

    close the modal window after creating a new method canvas.

--- essencia-portal/src/main/java/org/uengine/essencia/portal/NewMethodPopup.java
+++ essencia-portal/src/main/java/org/uengine/essencia/portal/NewMethodPopup.java
@@ -1,9 +1,11 @@
 package org.uengine.essencia.portal;
 
 
+import org.metaworks.Remover;
 import org.metaworks.ServiceMethodContext;
 import org.metaworks.annotation.*;
 import org.metaworks.dwr.MetaworksRemoteService;
+import org.metaworks.widget.ModalWindow;
 import org.uengine.essencia.enactment.EssenceProcessDefinition;
 import org.uengine.essencia.model.PracticeDefinition;
 import org.uengine.essencia.util.KernelUtil;
@@ -90,6 +92,8 @@ public class NewMethodPopup {
         processResource.save(processDefinition);
 
         processResource.reopen();
+
+        MetaworksRemoteService.addReturn(new Remover(new ModalWindow()));
 
     }
 }
