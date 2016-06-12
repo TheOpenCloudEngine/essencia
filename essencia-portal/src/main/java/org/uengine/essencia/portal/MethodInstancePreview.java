71dc2a6226e3133a5349b92163988678bc66a9fc
jinyoung jang <jyjang@uengine.org>
2016-06-13 01:49:21 +0900

    Essencia explorer menu has been added.

--- essencia-portal/src/main/java/org/uengine/essencia/portal/MethodInstancePreview.java
+++ essencia-portal/src/main/java/org/uengine/essencia/portal/MethodInstancePreview.java
@@ -1,11 +1,15 @@
 package org.uengine.essencia.portal;
 
+import org.metaworks.annotation.AutowiredFromClient;
+import org.metaworks.annotation.Id;
+import org.metaworks.annotation.ServiceMethod;
 import org.metaworks.dwr.MetaworksRemoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.uengine.codi.mw3.model.Instance;
 import org.uengine.codi.mw3.model.InstancePreview;
+import org.uengine.codi.mw3.model.Session;
 import org.uengine.essencia.enactment.GameBoard;
 import org.uengine.kernel.ProcessInstance;
 import org.uengine.processmanager.ProcessManagerRemote;
@@ -19,8 +23,12 @@ import java.rmi.RemoteException;
 @Scope("prototype")
 public class MethodInstancePreview extends InstancePreview{
 
+    public MethodInstancePreview(){}
+
     @Override
     public void load(Instance instance) throws Exception {
+
+        if(!instance.getDefVerId().endsWith(".method")) return;
 
         ProcessInstance processInstance = processManagerRemote.getProcessInstance(instance.getInstId().toString());
 
@@ -28,9 +36,35 @@ public class MethodInstancePreview extends InstancePreview{
 
         MetaworksRemoteService.autowire(getGameBoard());
 
-        getGameBoard().load(processInstance, false);
-        getGameBoard().getMetaworksContext().setWhere(null); //small version
+        try {
+            getGameBoard().load(processInstance, false);
+            getGameBoard().getMetaworksContext().setWhere(null); //small version
+        }catch (Exception e){}
 
+        setInstId(instance.getInstId());
+
+    }
+
+    @Id
+    public Long getInstId() {
+        return instId;
+    }
+
+    public void setInstId(Long instId) {
+        this.instId = instId;
+    }
+
+    Long instId;
+
+    @ServiceMethod(mouseBinding = "drag")
+    public void drag(@AutowiredFromClient Session session){
+
+        Instance instance = new Instance();
+        instance.setInstId(getInstId());
+
+        session.setClipboard(instance);
+
+        MetaworksRemoteService.wrapReturn(session);
     }
 
     @Autowired
