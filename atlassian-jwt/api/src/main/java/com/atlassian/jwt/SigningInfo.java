1599ef6b031c15bedc0ac223c6146bdbf3374509
SeungpilPark <sppark@uengine.org>
2016-06-14 13:42:55 +0900

    Remove unused atlassian jwt

--- atlassian-jwt/api/src/main/java/com/atlassian/jwt/SigningInfo.java
+++
@@ -1,9 +0,0 @@
-package com.atlassian.jwt;
-
-/**
- * Encapsulates the algorithm and key/secret to be used for signing and verifying jwt tokens
- */
-public interface SigningInfo
-{
-    SigningAlgorithm getSigningAlgorithm();
-}
--- atlassian-jwt/api/src/main/java/com/atlassian/jwt/SymmetricSigningInfo.java
+++
@@ -1,6 +0,0 @@
-package com.atlassian.jwt;
-
-public interface SymmetricSigningInfo extends SigningInfo
-{
-    String getSharedSecret();
-}
