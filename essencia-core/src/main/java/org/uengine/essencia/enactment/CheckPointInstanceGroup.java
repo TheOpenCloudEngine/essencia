4fcdd78bbe6d95ce6be6621ebe009287bfeb9a7b
jinyoung jang <jyjang@uengine.org>
2016-12-07 09:39:13 +0900

    Check All and forward check event to labels to toggle the Checkbox - https://github.com/TheOpenCloudEngine/essencia/issues/10

--- essencia-core/src/main/java/org/uengine/essencia/enactment/CheckPointInstanceGroup.java
+++ essencia-core/src/main/java/org/uengine/essencia/enactment/CheckPointInstanceGroup.java
@@ -1,11 +1,15 @@
 package org.uengine.essencia.enactment;
 
+import org.metaworks.ContextAware;
+import org.metaworks.MetaworksContext;
+import org.metaworks.annotation.ServiceMethod;
+
 import java.util.List;
 
 /**
  * Created by jangjinyoung on 15. 12. 20..
  */
-public class CheckPointInstanceGroup {
+public class CheckPointInstanceGroup{
 
     String title;
         public String getTitle() {
@@ -31,5 +35,27 @@ public class CheckPointInstanceGroup {
     public void setCheckPointInstanceList(List<CheckPointInstance> checkPointInstanceList) {
         this.checkPointInstanceList = checkPointInstanceList;
     }
+
+    public CheckPointInstanceGroup() {
+    }
+
+    @ServiceMethod(target = ServiceMethod.TARGET_SELF, callByContent = true)
+    public void checkAll(){
+
+        setCheckedAll(!isCheckedAll());
+
+        if(checkPointInstanceList!=null)
+        for(CheckPointInstance checkPointInstance : checkPointInstanceList){
+            checkPointInstance.setChecked(checkedAll);
+        }
+    }
+
+    boolean checkedAll;
+        public boolean isCheckedAll() {
+            return checkedAll;
+        }
+        public void setCheckedAll(boolean checkedAll) {
+            this.checkedAll = checkedAll;
+        }
 
 }
